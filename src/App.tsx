92980f1d71a4e3a4181655c5e536ec5391f1eaa8
israelmitolu <israelmitolu@gmail.com>
2024-01-25 14:54:35 +0100

    Project setup ✨

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
